1ad2feaed22bfdb10995dbb0ac7f5a5fec234a49
smithgaryyveeee <ssmithgaryyee@icloud.com>
2025-02-01 20:40:01 -0500

    Update custom-header.js

--- custom-header.js
+++ custom-header.js
@@ -1,15 +1,28 @@
-// Modify HTTP response headers with a custom header
+const regex = /^https:\/\/.*$/; // This is the regular expression that matches all HTTPS URLs
 
-// This is the regular expression that matches all HTTPS URLs
-const regex = /^https:\/\/.*$/;
-
-if (regex.test($request.url)) {
+if (regex.test(request.url)) {
     // Modify the HTTP response headers
-    $response.headers['X-Modified-By'] = 'Quantumult-X';
+    const modifiedResponseHeaders = ['X-Modified-By'];
+
+    // Add custom header to response
+    modifiedResponseHeaders.push('Content-Type: text/html');
 
     // Return the modified response
-    $done($response);
+    callback(responseWithModifiedHeaders);
 } else {
     // No changes needed, continue processing the request as usual
-    $done();
+    callback();
 }
+
+function* handleRequest() {
+    let response = yield; // The regular http.createServer() function in Node.js accepts a callback function. In this case, we're using an arrow function with a more functional syntax.
+
+    // Process the incoming request and generate a modified response. Here, I've added the custom header 'Content-Type: text/html'.
+    const responseWithModifiedHeaders = makeHtmlString('<p>Hello from modified server!</p>');
+
+    yield responseWithModifiedHeaders;
+}
+
+const server = http.createServer(handleRequest);
+server.listen(3000, () => console.log('Server running on port 3000'));
+
