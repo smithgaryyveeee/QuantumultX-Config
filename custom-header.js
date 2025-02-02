a1e74525e9c401f70a067daa3b0b46d4e947f4db
smithgaryyveeee <ssmithgaryyee@icloud.com>
2025-02-01 20:33:44 -0500

    Update custom-header.js

--- custom-header.js
+++ custom-header.js
@@ -1,9 +1,15 @@
-// Example script to modify HTTP response headers with a custom header
+// Modify HTTP response headers with a custom header
+
+// This is the regular expression that matches all HTTPS URLs
 const regex = /^https:\/\/.*$/;
 
 if (regex.test($request.url)) {
+    // Modify the HTTP response headers
     $response.headers['X-Modified-By'] = 'Quantumult-X';
+
+    // Return the modified response
     $done($response);
 } else {
+    // No changes needed, continue processing the request as usual
     $done();
 }
